7bee4ca009f98833900c64a2e6168f9078524e84
Emil Karimov <emvakar@gmail.com>
2018-06-05 14:30:45 +0300

    Initial Commit

---
+++ ElegantSwitcher/ElegantSwitcher.h
@@ -0,0 +1,19 @@
+//
+//  ElegantSwitcher.h
+//  ElegantSwitcher
+//
+//  Created by Emil Karimov on 05.06.2018.
+//  Copyright © 2018 Emil Karimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElegantSwitcher.
+FOUNDATION_EXPORT double ElegantSwitcherVersionNumber;
+
+//! Project version string for ElegantSwitcher.
+FOUNDATION_EXPORT const unsigned char ElegantSwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElegantSwitcher/PublicHeader.h>
+
+
